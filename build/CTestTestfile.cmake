dae014111c812caa6f10561e970f8759597447df
Sama <s.daryanavard.1@research.gla.ac.uk>
2021-03-16 12:57:52 +0000

    deleted extra files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/natalia/disertation/datarecordings
-# Build directory: /home/natalia/disertation/datarecordings/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
